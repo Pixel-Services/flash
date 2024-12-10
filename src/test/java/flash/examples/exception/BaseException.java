534313c5671dcf64e7ded66b1c4a2f57c30bfef1
Relism <itzdrekkopvp@gmail.com>
2024-12-10 02:11:27 +0100

    Removed the testing framework awaiting to setup a new one.

--- src/test/java/flash/examples/exception/BaseException.java
+++
@@ -1,5 +0,0 @@
-package flash.examples.exception;
-
-public class BaseException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-}
--- src/test/java/flash/examples/exception/NotFoundException.java
+++
@@ -1,6 +0,0 @@
-package flash.examples.exception;
-
-public class NotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-}
-
